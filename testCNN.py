c2b8cdc126e53814ee8cbed6f0169167ca601c00
HugoD <hugo.daumain@edu.devinci.fr>
2023-02-07 21:19:38 +0100

    change

--- testCNN.py
+++ testCNN.py
@@ -13,7 +13,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 import os
-
+from tqdm import tqdm
+import random 
 
 from torch.utils.data import Dataset, DataLoader
 from torch.autograd import Variable
@@ -71,10 +72,15 @@ class MyDataset(Dataset):
                 
                         
                 
+        #If train = True we take RANDOMELY 80% of the dataset for the train dataset and the rest for the test dataset
+        #Take randomly 80% of a list 
         if train:
-            self.samples = self.samples[:int(0.8*len(self.samples))]
+            self.samples = [self.samples[i] for i in sorted(random.sample(range(len(self.samples)), int(len(self.samples)*0.8)))]
+            self.labels = [self.labels[i] for i in sorted(random.sample(range(len(self.labels)), int(len(self.labels)*0.8)))]
         else:
-            self.samples = self.samples[int(0.8*len(self.samples)):]
+            self.samples = [self.samples[i] for i in sorted(random.sample(range(len(self.samples)), int(len(self.samples)*0.2)))]
+            self.labels = [self.labels[i] for i in sorted(random.sample(range(len(self.labels)), int(len(self.labels)*0.2)))]
+        
 
 
 
@@ -84,33 +90,53 @@ class MyDataset(Dataset):
     def __getitem__(self, idx):
         sample = self.samples[idx]
         label = self.labels[idx]
-        print("tensor size : ", torch.Tensor(sample).size())
         return torch.Tensor(sample).unsqueeze(0), label
 
-#Create a pytorch Convolutional NN taking tensor of 5 frames of 121 features as input and output a tensor of 5 float (the probability of each class)
-#The input is a tensor of 5 frames of 121 features
-
-#Try another one because this one give me as output RuntimeError: Given input size: (6x1x117). Calculated output size: (6x0x58). Output size is too small 
-#I think it is because the size of the input is not a multiple of the size of the kernel
-#I tried to change the size of the kernel but it doesn't work
+#Create a pytorch Convolutional with one convolutionnal layer takking as input a tensor with one channel, 4 batch, and 5 frames of 121 features*
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 5)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+                padding=0
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=8,
+                out_channels=16,
+                kernel_size=2,
+                stride=1,
+                padding=1
+
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        #self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.fc1 = nn.Linear(944, 50)
+        self.fc2 = nn.Linear(50, 5)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.conv1(x)
+   
+        x = torch.flatten(x, 1)
+  
+        x =self.fc1(x)
+    
+        x = self.fc2(x)
+    
+        
+        
+        x = F.sigmoid(x)
+
         return x
 
 
@@ -118,56 +144,68 @@ class Net(nn.Module):
 
 
 
+
 #The output is a tensor of 5 float (the probability of each class)
+#Create train and test method using criterion CorssEntropyLoss
+#The optimizer is Adam with a learning rate of 0.001
+#The number of epoch is 10
+#The batch size is 4
+#The number of workers is 4
+#The drop_last is True
+#The model is saved in the file model.pt
+#The accuracy is printed at the end of the training
 
 
-def train(model, train_loader, optimizer, epoch):
+def train(model, device, train_loader, optimizer, epoch, criterion):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = Variable(data), Variable(target)
-        
+        data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-        loss = F.cross_entropy(output, target)
-        loss.backward()
+        loss = criterion(output, target)
         optimizer.step()
-        if batch_idx % 10 == 0:
+        if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.data[0]))
+                100. * batch_idx / len(train_loader), loss.item()))
 
-def test(model, test_loader):
+
+def test(model, device, test_loader, criterion):
     model.eval()
     test_loss = 0
     correct = 0
-    for data, target in test_loader:
-        data, target = Variable(data, volatile=True), Variable(target)
-        data = data.unsqueeze(1)
-        output = model(data)
-        test_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
-        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            print("output : ", output, "target : ", target)
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 def main():
-    train_dataset = MyDataset("/Users/hugo/ArcticProject/CNNMOVE/MucaMoveDataset/dataset ",True)
-    test_dataset = MyDataset("/Users/hugo/ArcticProject/CNNMOVE/MucaMoveDataset/dataset ",False)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_dataset = MyDataset("/home/hugo/Bureau/ARTICPROJECT/CNNMove/test/MucaMoveDataset/dataset ",True)
+    test_dataset = MyDataset("/home/hugo/Bureau/ARTICPROJECT/CNNMove/test/MucaMoveDataset/dataset ",False)
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4, drop_last=True)
     test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=4, drop_last=True)
     for i,v in enumerate(train_loader):
-        print(v[0].size())
-  
-    model = Net()
-    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+        print("train : ", v[1])
+    for i,v in enumerate(test_loader):
+        print("test: ", v[1])
+    model = Net().to(device)
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    #use tqdm to display the progress bar
+    for epoch in (range(50)):
+        train(model, device, train_loader, optimizer, epoch,criterion)
+        test(model, device, test_loader, criterion)
 
-    for epoch in range(1, 10):
-         train(model, train_loader, optimizer, epoch)
-         test(model, test_loader)
 
 
 if __name__ == '__main__':
